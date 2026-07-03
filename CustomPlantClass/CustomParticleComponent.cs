fc2194466dc4fce862b684809493b517c090dffd
daisyqd-gif <daisy.qd@gmail.com>
2026-07-02 17:19:04 -0700

    update 3.7 (7.2.2026)
    half

--- CustomPlantClass/CustomParticleComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace CustomPlantClass
-{
-    public class CustomParticle : MonoBehaviour
-    {
-        public void Die() => Destroy(gameObject);
-    }
-}
--- CustomPlantClass/GameAPP_Awake_Patch.cs
+++
@@ -1,7 +0,0 @@
-namespace CustomPlantClass
-{
-    public class GameAppInitBehaviour : MonoBehaviour
-    {
-        
-    }
-}
